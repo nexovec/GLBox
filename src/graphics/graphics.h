f83d1809b3f37b3ee5e424f1c2789b20a4c7a4be
nexovec <hraccesky@gmail.com>
2020-12-30 21:38:46 +0100

    VA, VBO structs unused, untested

--- src/graphics/graphics.h
+++ src/graphics/graphics.h
@@ -1,7 +1,19 @@
 #include "mymath/mymath.h"
 #include "stdint.h"
 // FIXME: leaks
+typedef struct VBLayout{
+    uint32_t count;
+    uint32_t attrNames[16];
+    uint32_t compCounts[16];
+    uint32_t types[16];
+    uint32_t offsets[16];
+    uint32_t stride;
+}VBLayout;
+VBLayout *VBLayout_init(VBLayout *vbl);
+void VBLayout_destroy(VBLayout *vbl);
 typedef struct VBO{
     uint32_t id;
-    Vec3f *data;
+    VBLayout *layout;
+    float *data;
 }VBO;
+VBO *VBO_init(VBO *vbo, VBLayout *vbl);
