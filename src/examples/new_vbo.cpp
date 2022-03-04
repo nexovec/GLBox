882f500d3404be42e4ef666ca7d175592ed92662
nexovec <hraccesky@gmail.com>
2022-03-04 15:11:51 +0100

    CRASHES #2

--- src/examples/new_vbo.cpp
+++ src/examples/new_vbo.cpp
@@ -37,7 +37,7 @@ New_Vbo_Example::New_Vbo_Example()
     glm::mat4 ortho = glm::ortho(0.f, (GLfloat)WIDTH, (GLfloat)HEIGHT, 0.f, 0.f, 1000.f);
     glUniformMatrix4fv(this->matrix_loc, 1, false, glm::value_ptr(ortho));
 
-    this->color_loc = glGetAttribLocation(this->program, "pos");
+    this->pos_loc = glGetAttribLocation(this->program, "pos");
     this->color_loc = glGetAttribLocation(this->program, "color");
 
     glGenVertexArrays(1, &this->vao);
@@ -47,22 +47,32 @@ New_Vbo_Example::New_Vbo_Example()
     glVertexAttribPointer(this->pos_loc, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
     glVertexAttribBinding(this->pos_loc, this->position_buffer_binding_point);
 
+    // FIXME: crashes:
+    // FIXME: crashes:
+    glEnableVertexAttribArray(this->pos_loc);
+    // glEnableVertexAttribArray(0);
+
     // glVertexAttribPointer(this->color_loc, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (const void *)(3 * sizeof(float)));
     glVertexAttribPointer(this->color_loc, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
     glVertexAttribBinding(this->color_loc, this->color_buffer_binding_point);
+
+    // FIXME: crashes:
+    glEnableVertexAttribArray(this->color_loc);
+    // glEnableVertexAttribArray(0);
 
     glGenBuffers(1, &this->vbo_indices.positions);
     glGenBuffers(1, &this->vbo_indices.colors);
 
     // FIXME: magic numbers
     glBindBuffer(GL_ARRAY_BUFFER,this->vbo_indices.positions);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), this->data_containers.positions, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), this->data_containers.positions, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, this->vbo_indices.colors);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), this->data_containers.colors, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), this->data_containers.colors, GL_STATIC_DRAW);
 }
 void New_Vbo_Example::update()
 {
+    glDisable(GL_CULL_FACE);
     glBindVertexBuffer(this->position_buffer_binding_point, this->vbo_indices.positions, 0, 3 * sizeof(float));
     glBindVertexBuffer(this->color_buffer_binding_point, this->vbo_indices.colors, 0, 3 * sizeof(float));
 
