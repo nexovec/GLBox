41e81bd17121fda2d1ca99ced2e56329bd201475
nexovec <hraccesky@gmail.com>
2022-10-27 23:50:09 +0200

    fixed build script

--- run.bat
+++ run.bat
@@ -2,7 +2,10 @@
 pushd %~dp0
 echo Y | rmdir .\bin /s /q
 call .\premake5.exe vs2022
-@REM call MSBuild.exe GLBox.sln -m -t:Clean
-call MSBuild.exe GLBox.sln -m
-call .\bin\Debug\Sandbox.exe
+if not defined DevEnvDir (
+    call vcvarsall.bat x64
+)
+@REM MSBuild.exe GLBox.sln -m -t:Clean
+call MSBuild.exe GLBox.sln -m /p:Platform="Win64" /p:Configuration=Release
+call .\bin\Release\Sandbox.exe
 popd
